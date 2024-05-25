3eebebc9cf3509e9899a31a5cd6d4afc9f9368a9
C0BY4s <vuk.zz@yahoo.com>
2024-05-25 20:46:09 +0200

    Add descending order feature

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from swap import swap_numbers
+from extras.desc import print_descending
 
-a = int(input("Enter first number (0-3): "))
-b = int(input("Enter second number (0-3): "))
+def main():
+    a = int(input("Enter first number (0-3): "))
+    b = int(input("Enter second number (0-3): "))
 
-swapped_a, swapped_b = swap_numbers(a, b)
-print(f"The two swapped numbers are: {swapped_a} and {swapped_b}")
+    swapped_a, swapped_b = swap_numbers(a, b)
+    print(f"The two swapped numbers are: {swapped_a} and {swapped_b}")
+
+    n = int(input("Enter a number to print descending order: "))
+    print("Natural descending order is:")
+    print_descending(n)
+
+if __name__ == "__main__":
+    main()
